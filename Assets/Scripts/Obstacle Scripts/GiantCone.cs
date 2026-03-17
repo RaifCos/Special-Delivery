db5547754e48e13ecf4cda1d34172927983f4207
RaifCos <raifcostellobusiness@gmail.com>
2026-03-17 12:59:15 +0000

    Dynamic Giant Cone Positioning

--- Assets/Scripts/Obstacle Scripts/GiantCone.cs
+++ Assets/Scripts/Obstacle Scripts/GiantCone.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 // Script to handle the behaviour of the Giant Cone.
 public class GiantCone : MonoBehaviour {
 
+    [SerializeField] private Vector2 topCorner;
+    [SerializeField] private Vector2 bottomCorner;
     void Start() {
         // set Position somewhere in the middle of the map. 
-        transform.position = new Vector3(Random.Range(0, 31), 20f, Random.Range(0, 31));
+        transform.position = new Vector3(Random.Range(topCorner.x, bottomCorner.x), 20f, Random.Range(topCorner.y, bottomCorner.y));
     }
 }
