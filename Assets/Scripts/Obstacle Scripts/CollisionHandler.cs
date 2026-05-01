ede016de8bbe5975a7c35906570c185b300273a8
RaifCos <raifcostellobusiness@gmail.com>
2026-05-01 18:15:38 +0100

    Update Music Track

--- Assets/Scripts/Obstacle Scripts/CollisionHandler.cs
+++ Assets/Scripts/Obstacle Scripts/CollisionHandler.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 // Script to handle collisions for Props (Signs, Cones, Bins, etc.)
 public class CollisionHandler : MonoBehaviour {
 
-    [SerializeField] bool destroy;
+    [SerializeField] readonly bool destroy;
     bool beganFading = false;
     
     private void OnCollisionEnter(Collision collision) {
-        // Check if Collisions with the Level Enviornment Count
         if (!beganFading) {
             // Shrink and Delete Object Shortly After Collision.
             StartCoroutine(GameManager.obstacleManager.ShrinkAndDestroy(gameObject, destroy));
