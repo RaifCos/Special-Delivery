ace65d0b08a8259fde30ca7f7954e675e22f575b
RaifCos <raifcostellobusiness@gmail.com>
2026-08-08 14:50:01 +0100

    River Respawn Mechanic

---
+++ Assets/Scripts/RespawnHandler.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RespawnHandler : MonoBehaviour {
+    private static WaitForSeconds _waitForSeconds1 = new WaitForSeconds(1f);
+    [SerializeField] private Image respawnPanel;
+    [SerializeField] private Transform[] respawnNodes;
+    [SerializeField] private Vector3[] respawnRotations;
+
+    private void OnTriggerEnter(Collider other) {
+        GameObject collisionGO = other.gameObject;
+
+        if (collisionGO.CompareTag("Player")) StartCoroutine(RespawnFade(collisionGO));
+        else if (collisionGO.CompareTag("Prop")) Destroy(collisionGO);
+        else { 
+            ResetObject(collisionGO); 
+            collisionGO.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            collisionGO.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+    }
+
+    private IEnumerator RespawnFade(GameObject playerObj) {
+        PlayerControl pc = playerObj.GetComponent<PlayerControl>();
+        pc.SetState(false);
+        pc.StopVan();
+
+        int alpha = 0;
+        while (alpha < 255) {
+            alpha += 5;
+            respawnPanel.color = new Color(0, 0, 0, alpha / 255f);
+            yield return null;
+        } 
+        
+        ResetObject(playerObj);
+        yield return _waitForSeconds1;
+
+        while (alpha > 0) {
+            alpha -= 5;
+            respawnPanel.color = new Color(0, 0, 0, alpha / 255f);
+            yield return null;
+        } pc.SetState(true);
+    }
+
+    private void ResetObject(GameObject obj) {
+        // Identify Closest Respawn Node to Object.
+        Vector3 target = respawnNodes[0].position;
+        foreach(Transform node in respawnNodes) {
+            if (Vector3.Distance(obj.transform.position, node.position) < Vector3.Distance(obj.transform.position, target)) {
+                target = node.position;
+            }
+        }
+
+        // Identify Closest Respawn Rotation to Object.
+        Vector3 targetRotation = respawnRotations[0];
+        foreach(Vector3 rotation in respawnRotations) {
+            if (Vector3.Distance(obj.transform.eulerAngles, rotation) < Vector3.Distance(obj.transform.eulerAngles, targetRotation)) {
+                targetRotation = rotation;
+            }
+        }
+
+        obj.transform.SetPositionAndRotation(target, Quaternion.Euler(targetRotation));
+    }
+}
+
