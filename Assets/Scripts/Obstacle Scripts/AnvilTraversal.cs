08a86a8df08c2fadacfe6932ff1fb4d0b5208033
RaifCos <raifcostellobusiness@gmail.com>
2026-08-08 22:33:01 +0100

    Falling Anvil/Piano Sound Effect

--- Assets/Scripts/Obstacle Scripts/AnvilTraversal.cs
+++ Assets/Scripts/Obstacle Scripts/AnvilTraversal.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 // Script to handle the behaviour of the Anvil, Piano, and Fake Parcels.
 public class AnvilTraversal : MonoBehaviour {
     public float startHeight;
+    [SerializeField] private AudioClip fallSound;
+    [SerializeField] private bool fallingSoundPlayed;
 
     // Set initial height based on public variable.
     void OnEnable() { 
@@ -13,8 +15,15 @@ public class AnvilTraversal : MonoBehaviour {
     // While object is still falling, move position to fall in front of the camera (where the player is). 
     void FixedUpdate() {
         Vector3 target = Camera.main.transform.position + (Camera.main.transform.forward * 40);
-        if (transform.position.y > 20) {
+        float y = transform.position.y;
+
+        if (y > 20) {
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.x, transform.position.y, target.z), 20f);
+        }
+        
+        if (y < 120 && !fallingSoundPlayed) {
+            fallingSoundPlayed = true;
+            GameManager.audioManager.PlaySpatialSoundEffect(fallSound, target, 0f, true);
         }
     }
 }
