1887762949fcc9b78b73c82ca208a23bf06517c8
RaifCos <raifcostellobusiness@gmail.com>
2026-09-13 23:02:59 +0100

    Adjust Model Rotation Speed

--- Assets/Scripts/UI Scripts/UnlockModelRotate.cs
+++ Assets/Scripts/UI Scripts/UnlockModelRotate.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class UnlockModelRotate : MonoBehaviour {
 
-    private readonly float speed = 2f;
+    private readonly float speed = 1.5f;
 
     void FixedUpdate() { transform.Rotate(new Vector3(0f, speed, 0f)); }
 }
