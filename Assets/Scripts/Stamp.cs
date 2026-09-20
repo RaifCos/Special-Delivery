f78effbc4dff9cdb18f68189b2e5da41f8f0449f
RaifCos <raifcostellobusiness@gmail.com>
2026-09-21 00:04:47 +0100

    Begin Adding Stamp Logic

--- Assets/Scripts/Stamp.cs
+++ Assets/Scripts/Stamp.cs
@@ -2,9 +2,26 @@ using UnityEngine;
 
 public class Stamp : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] private int stampNumber;
+    private bool collected = false;
+
+    private GameObject stampObject;
+
+    void Start() {
+        stampObject = transform.GetChild(0).gameObject;
+        // collected = GameManager.DataManager.IsStampCollected(stampNumber);
+    }
+
+    void ActivateStamp() { if (!collected) stampObject.SetActive(true); }
+
+    void DeactivateStamp() { stampObject.SetActive(false); }
+
+    void OnTriggerEnter(Collider other) {
+        GameObject triggerGO = other.gameObject;
+
+        if (!triggerGO.CompareTag("Player") || collected) return; // Ignore Non-Player Triggers
+        collected = true;
+        // GameManager.DataManager.StampCollected(stampNumber);
+        DeactivateStamp();
     }
 }
